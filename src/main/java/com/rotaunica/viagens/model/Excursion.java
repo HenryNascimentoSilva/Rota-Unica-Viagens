8c61683b58ddb9373b3c2536275345cec110acc8
henrynascimentosilva <henrynascimentosilva@gmail.com>
2025-12-13 22:18:49 -0300

    feat: creating excursion entity

---
+++ src/main/java/com/rotaunica/viagens/model/Excursion.java
@@ -0,0 +1,33 @@
+package com.rotaunica.viagens.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Excursion {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+    private String title;
+    private String shortDescription;
+    private String longDescription;
+    private String imageURL;
+    private BigDecimal basePrice;
+    private String location;
+    private Integer dateAndTime;
+    private List<String> includes;
+}
