699a24b31f2f4229f452a9d6e719b7e240cecf55
henrynascimentosilva <henrynascimentosilva@gmail.com>
2025-12-13 22:22:03 -0300

    fix: change dateAndTime type

--- src/main/java/com/rotaunica/viagens/model/Excursion.java
+++ src/main/java/com/rotaunica/viagens/model/Excursion.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -28,6 +29,6 @@ public class Excursion {
     private String imageURL;
     private BigDecimal basePrice;
     private String location;
-    private Integer dateAndTime;
+    private LocalDateTime dateAndTime;
     private List<String> includes;
 }
