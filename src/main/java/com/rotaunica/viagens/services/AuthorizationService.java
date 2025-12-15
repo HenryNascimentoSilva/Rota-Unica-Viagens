ace2fa6e685995b07437557da068f5d6602a5863
henrynascimentosilva <henrynascimentosilva@gmail.com>
2025-12-14 21:20:45 -0300

    feat: creating AuthorizationService

---
+++ src/main/java/com/rotaunica/viagens/services/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.rotaunica.viagens.services;
+
+import com.rotaunica.viagens.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    final
+    UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
